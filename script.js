20ab4d0ae4e1c3c152d75bd0fbf263215b630bcb
Krish743 <142522152+Krish743@users.noreply.github.com>
2023-11-11 21:26:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -56,8 +56,13 @@ elems.forEach(function(elem){
          
       }
     });
-
+   
     
 });
 
-
+Shery.mouseFollower({
+  //Parameters are optional.
+  skew: true,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
